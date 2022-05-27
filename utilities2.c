a178f02b666be899359aac650e0ba143cebf7c12
Terraski <matteoparigipro@hotmail.com>
2022-05-27 20:44:05 +0200

    Pipe Working | redirection to files needed

--- utilities2.c
+++ utilities2.c
@@ -6,7 +6,7 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 16:25:31 by maparigi          #+#    #+#             */
-/*   Updated: 2022/05/24 16:59:48 by maparigi         ###   ########.fr       */
+/*   Updated: 2022/05/27 20:43:24 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,4 @@ void	ft_free(char **tab)
 		free(tab[i]);
 	if (tab)
 		free(tab);
-}
-
-int	ft_strlenx(char *str, char x)
-{
-	int	i;
-	int	n;
-
-	n = 0;
-	i = -1;
-	while (str[++i])
-		if (str[i] != x)
-			n++;
-	return (n);
 }
