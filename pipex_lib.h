c29ab13a425986cd0fedc18cc852ebd6e4bbeb00
Terraski <matteoparigipro@hotmail.com>
2022-05-27 20:51:50 +0200

    Pipe Working | redirection to files needed

--- pipex_lib.h
+++ pipex_lib.h
@@ -6,7 +6,7 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 21:55:39 by maparigi          #+#    #+#             */
-/*   Updated: 2022/05/27 19:31:42 by maparigi         ###   ########.fr       */
+/*   Updated: 2022/05/27 20:51:31 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,11 @@ char	*find_apath(char **env);
 
 char	**ft_split(char const *s, char c);
 
-char	*ft_strjoin(char const *s1, char const *s2);
 char	*add_path(char *str, char *av1, char c);
 char	*access_path(char **paths, char *cmd);
 char	*check_access(char **env, char *cmd);
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_strlenx(char *str, char x);
 
 size_t	ft_strlen(const char *s);
 
