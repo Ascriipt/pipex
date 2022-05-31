036910761534ac47a803e47af133bf4c5c11a618
Ascriipt <maparigi@student.42.fr>
2022-05-31 17:51:38 +0200

    in/out file

--- main.c
+++ main.c
@@ -6,40 +6,41 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 21:54:59 by maparigi          #+#    #+#             */
-/*   Updated: 2022/05/31 17:34:05 by maparigi         ###   ########.fr       */
+/*   Updated: 2022/05/31 17:50:13 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_lib.h"
 
-void	child_proc(char **env, char **cmdargs, char *str, int pfd[2])
+void	child_proc(char **env, char **cmdargs, int pfd[2])
 {
+	char	*str;
+
+	str = check_access(env, cmdargs[0]);
 	if (dup2(pfd[1], STDOUT_FILENO) == -1)
-	{
-		perror("dup2 : child ");
 		exit(EXIT_FAILURE);
-	}
 	close(pfd[0]);
 	execve(str, cmdargs, env);
 	close(pfd[1]);
+	free(str);
 }
 
-void	parent_proc(char **env, char **cmdargs, char *str, int pfd[2])
+void	parent_proc(char **env, char **cmdargs, int pfd[2])
 {
+	char	*str;
+
+	str = check_access(env, cmdargs[0]);
 	if (dup2(pfd[0], STDIN_FILENO) == -1)
-	{
-		perror("dup2 : parent ");
 		exit(EXIT_FAILURE);
-	}
 	close(pfd[1]);
 	execve(str, cmdargs, env);
 	close(pfd[0]);
+	free(str);
 }
 
 static void	ft_procs(char **argv, char **env, int *pfd, int pid1)
 {
 	char	**cmdargs;
-	char	*str;
 
 	pid1 = fork();
 	if (pid1 < 0)
@@ -47,19 +48,18 @@ static void	ft_procs(char **argv, char **env, int *pfd, int pid1)
 	if (pid1 == 0)
 	{
 		cmdargs = ft_split(argv[1], 32);
-		str = check_access(env, cmdargs[0]);
-		child_proc(env, cmdargs, str, pfd);
+		if (!cmdargs[0])
+			exit(EXIT_FAILURE);
+		child_proc(env, cmdargs, pfd);
 	}
 	else
 	{
 		cmdargs = ft_split(argv[2], 32);
 		if (!cmdargs[0])
 			exit(EXIT_FAILURE);
-		str = check_access(env, cmdargs[0]);
 		waitpid(pid1, NULL, 0);
-		parent_proc(env, cmdargs, str, pfd);
+		parent_proc(env, cmdargs, pfd);
 	}
-	free(str);
 	ft_free(cmdargs);
 }
 
