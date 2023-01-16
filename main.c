a393cff6071508cd8f2aa67bcb6d0c48f3d8e526
maparigi <maparigi@student.42.fr>
2023-01-16 01:29:56 +0100

    protected for unknown files

--- main.c
+++ main.c
@@ -6,11 +6,23 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 21:54:59 by maparigi          #+#    #+#             */
-/*   Updated: 2023/01/12 19:15:13 by maparigi         ###   ########.fr       */
+/*   Updated: 2023/01/16 01:29:04 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_lib.h"
+
+static void	command_nfound(char **cmdargs)
+{
+	if (cmdargs[0])
+	{
+		write(STDERR_FILENO, "command not found: ", 19);
+		write(STDERR_FILENO, cmdargs[0], ft_strlen(cmdargs[0]));
+		write(STDERR_FILENO, "\n", 1);
+	}
+	ft_free(cmdargs);
+	exit(EXIT_FAILURE);
+}
 
 void	child_proc(char **argv, char **env, char **cmdargs, int pfd[2])
 {
@@ -19,6 +31,13 @@ void	child_proc(char **argv, char **env, char **cmdargs, int pfd[2])
 
 	str = check_access(env, cmdargs[0]);
 	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
+	{
+		free(str);
+		perror(argv[1]);
+	}
+	if (str == NULL)
+		command_nfound(cmdargs);
 	if (dup2(file1, STDIN_FILENO) == -1)
 	{
 		ft_free(cmdargs);
@@ -42,6 +61,12 @@ void	parent_proc(char **argv, char **env, char **cmdargs, int pfd[2])
 	str = check_access(env, cmdargs[0]);
 	if (file2 == -1 || str == NULL)
 	{
+		if (str == NULL)
+		{
+			write(STDERR_FILENO, "command not found: ", 19);
+			write(STDERR_FILENO, cmdargs[0], ft_strlen(cmdargs[0]));
+			write(STDERR_FILENO, "\n", 1);
+		}
 		free(str);
 		ft_free(cmdargs);
 		exit(EXIT_FAILURE);
@@ -67,14 +92,14 @@ static void	ft_procs(char **argv, char **env, int *pfd, int pid1)
 	{
 		cmdargs = ft_split(argv[2], 32);
 		if (!cmdargs[0])
-			exit(EXIT_FAILURE);
+			command_nfound(cmdargs);
 		child_proc(argv, env, cmdargs, pfd);
 	}
 	else
 	{
 		cmdargs = ft_split(argv[3], 32);
 		if (!cmdargs[0])
-			exit(EXIT_FAILURE);
+			command_nfound(cmdargs);
 		waitpid(pid1, NULL, WNOHANG);
 		parent_proc(argv, env, cmdargs, pfd);
 	}
@@ -86,7 +111,7 @@ int	main(int argc, char **argv, char **env)
 	pid_t	pid1;
 	int		pfd[2];
 
-	if (argc < 5 || !env[0] || !env)
+	if (argc < 5 || !env || !env[0])
 		return (1);
 	if (pipe(pfd) == -1)
 	{
